0ae098a395b87a3673ac2e84d68cbbd519540533
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2019-11-11 19:16:53 +0100

    Add validations in user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+# == Validations =================================================
+  validates :first_name, :last_name, :age, :email, presence: true
+  validates :email, uniqueness: true
 end
