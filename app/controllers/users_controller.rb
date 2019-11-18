b8f693234817465cd40c80ae76f9d5d04978053e
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2019-11-18 06:45:33 +0100

    Add create method to our CRUD.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,27 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
   def show; end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      redirect_to users_path
+    end
+  end
 
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :age, :email)
   end
 end
