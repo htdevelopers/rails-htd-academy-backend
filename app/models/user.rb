30b366f980a81d98cad0f01556af1802cab8c5a8
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2019-11-18 23:38:29 +0100

    Add role to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
 
-# == Validations =================================================
+  # == Validations =================================================
   validates :first_name, :last_name, :age, :email, presence: true
   validates :email, uniqueness: true
+
+  # == Enums =======================================================
+  enum role: {
+    patient: 0,
+    doctor: 1,
+    admin: 2
+  }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_171637) do
+ActiveRecord::Schema.define(version: 2019_11_18_223125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_11_11_171637) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0, null: false
   end
 
 end
