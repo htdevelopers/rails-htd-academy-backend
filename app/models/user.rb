a2048accf6d41ccde8bda37185d76eb0ae98e475
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2019-11-19 06:42:52 +0100

    Create helpful 'full_name' user model method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User < ApplicationRecord
     admin: 2
   }
 
+  # == Methods ======================================================
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
