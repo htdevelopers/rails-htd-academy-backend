efb902db97b9accefdace4ebd15c06ce2aa04f03
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2019-11-19 06:41:28 +0100

    Configure controllers.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @patients = User.patient
     @doctors = User.doctor
