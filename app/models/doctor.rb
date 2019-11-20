40b249458af02a1f2f1f61cbb1d51014c76991f4
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2019-11-21 00:03:24 +0100

    Create model validations.

---
+++ app/models/doctor.rb
@@ -0,0 +1,12 @@
+class Doctor < ApplicationRecord
+
+  validates :first_name, :last_name, :practicing_from, :qualification_number, :employment, presence: true
+  validates_uniqueness_of :qualification_number
+
+  enum employment: {
+    full_time: 0,
+    part_time: 1,
+    intern: 2,
+    contract: 3
+  }
+end
