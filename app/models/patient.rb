1070cee5aeafdcea0ac584f728d2ec6288f5c64b
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2019-11-20 23:51:06 +0100

    Create first specs.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,8 +3,8 @@ class Patient < ApplicationRecord
   validates :first_name, :last_name, :age, presence: true
 
   enum gender: {
-    male: 1,
-    female: 2,
-    other: 3
+    male: 0,
+    female: 1,
+    other: 2
   }
 end
