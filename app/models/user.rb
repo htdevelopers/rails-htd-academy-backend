22ae220f873a2af72b14dc6b5aa40fb30a7225e6
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2019-11-21 12:08:54 +0100

    Add attachment in user model and permit avatar params in app controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+
   validates :first_name, :last_name, presence: true
 
   def full_name
