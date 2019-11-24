4b222a8fdb23c1a738bd948dfc47c1c9104e2a1d
Tomasz Kot <kottomasz98@gmail.com>
2019-11-24 11:31:35 +0100

    Add CRUD actions for patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,7 +1,56 @@
 # frozen_string_literal: true
 
 class PatientsController < ApplicationController
+  before_action :set_patient, only: [:edit, :update, :show, :destroy]
+
   def index
     @patients = Patient.all.includes(:doctor)
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def edit; end
+
+  def update
+    if @patient.update(patient_params)
+      flash[:notice] = 'Patient successfully updated'
+      redirect_to patients_path
+    else
+      flash[:error] = 'Patient cannot be updated'
+      redirect_to edit_patient_path(@patient)
+    end
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+
+    if @patient.save
+      flash[:notice] = 'Patient successfully created'
+      redirect_to patients_path
+    else
+      flash[:error] = 'Patient cannot be created'
+      render :new
+    end
+  end
+
+  def destroy
+    msg = @patient.destroy ? { notice: 'Patient successfully deleted' } : { error: 'Patient cannot be deleted' }
+    redirect_to patients_path, msg
+  end
+
+  def show; end
+
+  private
+
+  def set_patient
+    @patient = Patient.find(params[:id])
+  end
+
+  def patient_params
+    params.require(:patient).permit(:first_name, :last_name,
+                                    :age, :gender
+                                  )
   end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: redirect('/patients')
-  resources :patients, only: :index
+  resources :patients
   resources :doctors, only: :index
 end
